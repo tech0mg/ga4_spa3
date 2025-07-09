a23a025a61299369e58e69baf03e682884c3c848
mukagen-mac <tech0.mukainakano@gmail.com>
2025-07-10 08:04:33 +0900

    初回ファイル作成

---
+++ lib/gtag.ts
@@ -0,0 +1,22 @@
+export const GA_TRACKING_ID = 'G-R01K8JTE16' // ここにあなたのGA4トラッキングIDを設定してください
+
+// https://developers.google.com/analytics/devguides/collection/gtagjs/pages
+export const pageview = (url: URL) => {
+  window.gtag('config', GA_TRACKING_ID, {
+    page_path: url,
+  })
+}
+
+// https://developers.google.com/analytics/devguides/collection/gtagjs/events
+export const event = ({ action, category, label, value }: {
+  action: string;
+  category: string;
+  label: string;
+  value: number;
+}) => {
+  window.gtag('event', action, {
+    event_category: category,
+    event_label: label,
+    value: value,
+  })
+}
